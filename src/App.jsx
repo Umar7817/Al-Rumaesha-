5826a55ca260bc782c819c2e4dc09b2107b0afff
Umar <us42534@gmail.com>
2024-01-13 07:33:28 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+
+function App() {
+  
+
+  return (
+    <> 
+     {/* <Nav /> */}
+     <Main />
+     {/* <Information /> */}
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Global.css";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <section className="contact-us">
+          <h2>Contact Us</h2>
+          <p>
+            Have questions or ready to book your Hajj tour? Reach out to us:
+          </p>
+          <address>
+            Email: alrumaesahajjtoursamd@gmail.com
+            <br />
+            Mohamed Faishel - 8140311997
+            <br />
+            Mohamed Furkan - 7990710370
+            <br />
+            Address : 526 5th floor Sheetal Varsha Mahavir business park opp Bukhari bawa dargah behrampura Ahmedabad 380022
+          </address>
+        </section>
+        <p>
+          &copy; {new Date().getFullYear()} Al-Rumaesha. All rights reserved.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
